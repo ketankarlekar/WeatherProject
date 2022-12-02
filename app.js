c6959badb2b8b94dca6fb5f1ddb1224aa1c0ccae
ketan Karlekar <karlekarketan@gmail.com>
2022-12-02 22:19:21 +0530

    final touchup

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const { response } = require("express");
+// const { response } = require("express");
+
+
 const express = require("express");
-const { read } = require("fs");
+// const { read } = require("fs");
 const app = express();
 const https = require("https");
 const bodyParser = require("body-parser")
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
@@ -13,39 +18,50 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", (req, res) => {
-    console.log("post request received");
+
+
+
+    const query = req.body.cityName;
+    const apiKey = "d1d225605aabf4289d727f5be4fc0008";
+    const units = "metric";
+
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + units + "&appid=" + apiKey + ""
+
+    https.get(url, function(response) {
+        console.log(response.statusCode);
+
+
+        response.on("data", function(data) {
+
+            const weatherData = JSON.parse(data);
+            console.log(weatherData);
+
+            const temp = weatherData.main.temp;
+            console.log("temp " + temp);
+
+            const visiblity = weatherData.visibility;
+            console.log("visibility " + visiblity);
+
+            const discription = weatherData.weather[0].description;
+            console.log(discription);
+
+            const icon = weatherData.weather[0].icon;
+
+            const imgUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+
+            res.write("<p>The weather is currently " + discription + "<P>");
+            res.write("<h1>The tempture in " + query + "  is " + temp + " degree Celsius °C</h1>");
+            res.write("<img src=" + imgUrl + ">");
+            res.send();
+        });
+    });
+
 })
 
 
 
-// const query = "pune";
-// const apiKey = "d1d225605aabf4289d727f5be4fc0008";
-// const units = " metric ";
-
-
-// const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + units + "&appid=" + apiKey + ""
-
-// https.get(url, function(response) {
-//     console.log(response.statusCode);
-
-
-//     response.on("data", function(data) {
-//         const weatherData = JSON.parse(data);
-//         console.log(weatherData);
-//         const temp = weatherData.main.temp;
-//         console.log("temp " + temp);
-//         const visiblity = weatherData.visibility;
-//         console.log("visibility " + visiblity);
-//         const discription = weatherData.weather[0].description;
-//         console.log(discription);
-//         const icon = weatherData.weather[0].icon;
-//         const imgUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-//         res.write("<p>The weather is currently " + discription + "<P>");
-//         res.write("<h1>The tempture in " + query + "  is " + temp + " fahranite</h1> ");
-//         res.write("<img src=" + imgUrl + ">");
-//         res.send();
-//     });
-// });
 
 
 
